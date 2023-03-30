d020d8c3229134bfdec0008eb97ec9dc7e33a322
Brigitte <114067290+brishar0n@users.noreply.github.com>
2023-03-30 22:49:40 +0700

    Update TowerOfHanoiRecurive.java

--- Assignment Forum Week 6 - Tower of Hanoi/TowerOfHanoiRecurive.java
+++ Assignment Forum Week 6 - Tower of Hanoi/TowerOfHanoiRecurive.java
@@ -1,1 +1,25 @@
+import java.util.*;
 
+public class TowerOfHanoiRecursive {
+
+    public static String showMoves(int n, char startPeg,
+                                   char destPeg, char tempPeg) {
+        if (n == 1) {
+            return "Move disk 1 from peg " + startPeg +
+                    " to peg " + destPeg + "\n";
+        } else { // recursive implementation
+            return showMoves(n-1, startPeg, tempPeg, destPeg)
+                    + "Move disk " + n + " from peg " + startPeg
+                    + " to peg " + destPeg + "\n"
+                    +showMoves(n-1, tempPeg, destPeg, startPeg);
+        }
+    }
+
+    public static void main (String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.println("Enter number of disks: ");
+        int nDisks = in.nextInt ();
+        String moves = showMoves (nDisks, 'L', 'R', 'M');
+        System.out.println(moves);
+    }
+}
